0d51c85ad9d5c5ef2bf861436a28cebcc97143e5
Glitch-bug <kwabenayiadom271@gmail.com>
2025-07-09 15:03:24 +0000

    a minor fix to budgets

--- src/bugdets/budgets.service.ts
+++ src/bugdets/budgets.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { BudgetsRepository } from './budgets.repository';
+import { Repository } from 'typeorm';
 import { CreateBudgetDto } from './dto/create-budget.dto';
 import { Budget } from './budget.entity';
 
 @Injectable()
 export class BudgetsService {
   constructor(
-    @InjectRepository(BudgetsRepository)
-    private budgetsRepository: BudgetsRepository,
+    // Inject the standard TypeORM repository for the Budget entity
+    @InjectRepository(Budget)
+    private readonly budgetsRepository: Repository<Budget>,
   ) {}
 
+  /**
+   * Creates multiple budget entries in the database.
+   * @param createBudgetDtos - An array of budget data transfer objects.
+   * @returns A promise that resolves to the array of created budget entities.
+   */
   async createBudgets(createBudgetDtos: CreateBudgetDto[]): Promise<Budget[]> {
-    return this.budgetsRepository.createBudgets(createBudgetDtos);
+    // Use the .create() method from the injected repository to prepare the entities
+    const budgets = this.budgetsRepository.create(createBudgetDtos);
+    
+    // Use the .save() method to insert the new entities into the database
+    await this.budgetsRepository.save(budgets);
+    
+    return budgets;
   }
 }
