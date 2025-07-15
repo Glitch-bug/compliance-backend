85b29947798b9e3365126aa998a8fe6f5f533bcf
Glitch-bug <kwabenayiadom271@gmail.com>
2025-07-15 14:30:58 +0000

    again

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
+import { BudgetsModule } from '../budgets/budgets.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    // This breaks the dependency cycle where UsersModule needs BudgetsModule
+    forwardRef(() => BudgetsModule),
+  ],
   providers: [UsersRepository, UsersService],
-  exports: [UsersRepository, UsersService], // Export repository for use in Auth module
+  exports: [UsersRepository, UsersService],
 })
 export class UsersModule {}
