6447474ec77ab8240dd13a4583806c2909f67817
Glitch-bug <kwabenayiadom271@gmail.com>
2025-08-25 23:06:51 +0000

    last thing

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -5,9 +5,10 @@ import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
 import { AuthModule } from '../auth/auth.module';
 import { ActiveChecklist } from 'src/checklists/entity/active-checklist.entity';
+import { ChecklistTemplate } from 'src/checklists/entity/checklist-template.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RequestEntity, ActiveChecklist]), AuthModule,],
+  imports: [TypeOrmModule.forFeature([RequestEntity, ActiveChecklist, ChecklistTemplate]), AuthModule,],
   controllers: [RequestsController],
   providers: [RequestsService],
 })
