25df0590f4fdb1befce9ed0df0c5b6590ef0b25b
Glitch-bug <kwabenayiadom271@gmail.com>
2025-08-26 17:01:12 +0000

    kelp

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -12,7 +12,7 @@ import { User } from 'src/users/user.entity';
 
 @Controller('requests')
 export class RequestsController {
-  constructor(private readonly requestsService: RequestsService) {}
+  constructor(private readonly requestsService: RequestsService) { }
 
   @Post()
   @UseGuards(AuthGuard(), RolesGuard)
@@ -27,7 +27,7 @@ export class RequestsController {
   findAll(@GetUser() user: User, @Query('mda') mda?: string) {
     return this.requestsService.findAll(user, mda);
   }
-  
+
   @Get('/review')
   @UseGuards(AuthGuard(), RolesGuard)
   @SetMetadata('roles', ['MoF Compliance', 'IAA Auditor', 'Minister'])
@@ -35,11 +35,23 @@ export class RequestsController {
     return this.requestsService.findForReview();
   }
 
+  @Get('/review/external')
+  @UseGuards(ApiKeyGuard)
+  findForReviewExternal() {
+    return this.requestsService.findForReview("external");
+  }
+
   @Patch(':id')
   @UseGuards(AuthGuard(), RolesGuard)
   @SetMetadata('roles', ['MoF Compliance', 'IAA Auditor', 'Minister', 'MDA'])
   update(@Param('id') id: string, @Body() updateRequestDto: UpdateRequestDto, @GetUser() user: User) {
     return this.requestsService.update(id, updateRequestDto, user);
+  }
+
+  @Patch('/external:id')
+  @UseGuards(ApiKeyGuard)
+  updateExternal(@Param('id') id: string, @Body() updateRequestDto: UpdateRequestDto,) {
+    return this.requestsService.updateExternal(id, updateRequestDto,);
   }
 
   @Delete(':id')
