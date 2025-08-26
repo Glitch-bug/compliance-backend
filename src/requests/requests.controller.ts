0611bd42468a8e5a4ff9d8334308bc5dd42dd566
Glitch-bug <kwabenayiadom271@gmail.com>
2025-08-26 10:45:59 +0000

    MDA

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -17,8 +17,7 @@ export class RequestsController {
   @Post()
   @UseGuards(AuthGuard())
   @UseGuards(RolesGuard)
-  @SetMetadata('roles', [Role.MDA])
-  // @SetMetadata('roles', ['MDA'])
+  @SetMetadata('roles', ['MDA'])
   create(@Body() createRequestDto: CreateRequestDto, @GetUser() user: User) {
     console.log(`Lets go people ${user}`);
     return this.requestsService.create(createRequestDto, user);
